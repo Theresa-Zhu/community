21458f6075ea5274daf785def5d19e4e0e16f4da
theresa_zhu <theresa_kaga@outlook.com>
2022-09-16 13:00:28 +0800

    add sql ddl

--- README.md
+++ README.md
@@ -6,6 +6,23 @@
 [es](https://elasticsearch.cn/explore)  
 [BootStrap](https://v3.bootcss.com/)  
 [Github OAuth](https://docs.github.com/cn/developers/apps/building-oauth-apps/creating-an-oauth-app)
+[Mybatis](http://mybatis.org/spring-boot-starter/mybatis-spring-boot-autoconfigure/)  
+[Spring Boot文档](https://docs.spring.io/spring-boot/docs/2.0.0.RC2/reference/htmlsingle/)  
+
+
 ## 工具
 [Git](https://git-scm.com/downloads)  
 
+##脚本
+```sql
+    CREATE TABLE USER(
+        "ID" INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+        "ACCOUNT_ID" VARCHAR(100),
+        "NAME" VARCHAR(50),
+        "TOKEN" CHAR(36),
+        "GMT_CREATE" BIGINT,
+        "GMT_MODIFIED" BIGINT
+    )
+    
+
+```
--- src/main/java/com/web/community/controller/AuthorizeController.java
+++ src/main/java/com/web/community/controller/AuthorizeController.java
@@ -1,13 +1,19 @@
-package com.web.community.Controller;
+package com.web.community.controller;
 
 import com.web.community.dto.AccessTokenDTO;
 import com.web.community.dto.GithubUser;
+import com.web.community.mapper.UserMapper;
+import com.web.community.model.User;
 import com.web.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -16,23 +22,40 @@ public class AuthorizeController {
 
     @Value("${github.client.id}")
     private String clientId;
+
     @Value("${github.client.secret}")
     private String clientSecrect;
+
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
-    public String callBack(@RequestParam(name = "code")String code){
+    public String callBack(@RequestParam(name = "code")String code,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecrect);
         accessTokenDTO.setCode(code);
         accessTokenDTO.setRedirect_url(redirectUri);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if (githubUser != null){
+            //登录成功，写cookie 和 session
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            request.getSession().setAttribute("user",githubUser);
+            return "redirect:/";
+        }else {
+            //登录失败，重新登录
+            return "redirect:/";
+        }
     }
-
-
 }
