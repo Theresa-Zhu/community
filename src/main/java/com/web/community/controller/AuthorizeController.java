d18a102a059e2d511ec3022f04e194f9cdcdad12
theresa_zhu <theresa_kaga@outlook.com>
2022-09-16 14:17:19 +0800

    add check cookies

--- src/main/java/com/web/community/controller/AuthorizeController.java
+++ src/main/java/com/web/community/controller/AuthorizeController.java
@@ -12,7 +12,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -34,7 +36,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callBack(@RequestParam(name = "code")String code,
-                           HttpServletRequest request){
+                           HttpServletRequest request,
+                           HttpServletResponse response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecrect);
@@ -42,16 +45,18 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_url(redirectUri);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser githubUser = githubProvider.getUser(accessToken);
-        if (githubUser != null){
-            //登录成功，写cookie 和 session
+        if (githubUser != null){  //获取github的用户信息
+            //登录成功
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
-            userMapper.insert(user);
-            request.getSession().setAttribute("user",githubUser);
+            userMapper.insert(user); //把该用户信息写入数据库中
+            //写cookie 和 session
+            response.addCookie(new Cookie("token",token));
             return "redirect:/";
         }else {
             //登录失败，重新登录
--- src/main/java/com/web/community/controller/IndexController.java
+++ src/main/java/com/web/community/controller/IndexController.java
@@ -1,13 +1,34 @@
 package com.web.community.controller;
 
+import com.web.community.mapper.UserMapper;
+import com.web.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+
 @Controller
 public class IndexController {
+    @Autowired
+    private UserMapper userMapper;
 
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for(Cookie cookie : cookies){
+            if (cookie.getName().equals("token")){
+                String token = cookie.getValue();//请求拿到 key为token的值
+                User user = userMapper.findByToken(token);//查找相对应的用户
+                if (user != null){
+                    //如果不为空，就返回该用户的信息，否则展示为登录
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
         return "index";
     }
 }
